4b59e7cbc0635c1366259d0eb760d0088b294e00
britton_laptop <brittonopetersen@gmail.com>
2024-11-09 10:32:16 +0200

    Added some new wallpapers

--- nvim/.config/nvim/lua/plugins/general.lua
+++ nvim/.config/nvim/lua/plugins/general.lua
@@ -1,5 +1,4 @@
 return {
-	
 	'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- "gc" to comment visual regions/lines
@@ -34,26 +33,41 @@ return {
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+{
+  -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require('which-key').setup()
 
-     -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
-  },
+    -- Document existing key chains
+    require('which-key').register {
+      { "<leader>c", group = "[C]ode" },  -- Defines a group for <leader>c
+      { "<leader>c_", hidden = true },    -- Hides the rest of the <leader>c mappings
+
+      { "<leader>d", group = "[D]ocument" },  -- Defines a group for <leader>d
+      { "<leader>d_", hidden = true },    -- Hides the rest of the <leader>d mappings
+
+      { "<leader>r", group = "[R]ename" },  -- Defines a group for <leader>r
+      { "<leader>r_", hidden = true },    -- Hides the rest of the <leader>r mappings
+
+      { "<leader>s", group = "[S]earch" },  -- Defines a group for <leader>s
+      { "<leader>s_", hidden = true },    -- Hides the rest of the <leader>s mappings
+
+      { "<leader>w", group = "[W]orkspace" },  -- Defines a group for <leader>w
+      { "<leader>w_", hidden = true },    -- Hides the rest of the <leader>w mappings
+
+      { "<leader>t", group = "[T]oggle" },  -- Defines a group for <leader>t
+      { "<leader>t_", hidden = true },    -- Hides the rest of the <leader>t mappings
+
+      { "<leader>h", group = "Git [H]unk" },  -- Defines a group for <leader>h
+      { "<leader>h_", hidden = true },    -- Hides the rest of the <leader>h mappings
+    }
+
+    -- Visual mode
+    require('which-key').register({
+      { "<leader>h", desc = "Git [H]unk", mode = "v" },  -- Describes <leader>h in visual mode
+    })
+  end,
+},
 }
